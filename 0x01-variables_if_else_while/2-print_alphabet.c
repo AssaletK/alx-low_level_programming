cf66291ba2b89a17c159b9e996fa8b56cd08264e
Assaletk <assaletkoua@icloud.com>
2022-07-01 22:44:48 +0000

    Print alphabet with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*
+* main - main function
+*
+* return 0
+*/
+
+int main(void)
+{
+	char c = 97;
+
+	while ( c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
