ba00f0d0d059eb80c5ad133c4b1900cbbf7d4079
Assaletk <assaletkoua@icloud.com>
2022-07-01 23:04:57 +0000

    Print alphabet in lowercase and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,12 @@ int main(void)
 	char c = 97;
 	char C = 65;
 
-	while ( c <= 122 )
+	while (c <= 122)
 	{
 		putchar(c);
 		c++;
 	}
-	while ( C <= 90 )
+	while (C <= 90)
 	{
 		putchar(C);
 		C++;
