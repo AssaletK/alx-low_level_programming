3cb52334185bc24c5133a782cdaf5c81d790cecf
Assaletk <assaletkoua@icloud.com>
2022-07-03 22:50:00 +0000

    Print combinaison of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,34 +4,37 @@
 *
 * Return: 0
 */
+
 int main(void)
 {
 	int i, j, k;
 
+	j = 48;
+	k = 48;
 	i = 48;
+
 	while (i < 58)
 	{
 		j = 48;
 		while (j < 58)
 		{
-			if (i != j && i < j)
+			k = 48;
+			while (k < 58)
 			{
-				putchar(i);
-				putchar(j);
-				k = 48;
-				while (k < 58)
+				if (i != j && j != k && i < j && j < k)
 				{
-					if (j != k && j < k)
-					{
-						putchar(k);
-					}
+					putchar(i);
+					putchar(j);
+					putchar(k);
 					if (i == 55 && j == 56 && k == 57)
 					{
 						break;
 					}
+					putchar(44);
+					putchar(32);
 				}
-				k++;
 			}
+			k++;
 		}
 		j++;
 	}
