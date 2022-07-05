c26a5a334c0621c80232b319b258aac32a099866
Assaletk <assaletkoua@icloud.com>
2022-07-05 22:43:16 +0000

    compute the absolute value of an interger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _abs - basolute value
+*
+* @n: is an integer
+*
+* Return: always 0
+*/
+
+int _abs(int n)
+{	
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		n *= -1;
+		return (n);
+	}
+}
