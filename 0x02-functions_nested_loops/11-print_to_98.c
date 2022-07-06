7dc5a2366ebb81591ac3bc7326507638b0860eec
Assaletk <assaletkoua@icloud.com>
2022-07-06 01:12:14 +0000

    print number up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - print to 98
+*
+* @n: is an integer
+*
+* Return: always 0
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i == 98)
+			{	
+				printf("\n");
+				break;
+			}
+			printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i == 98)
+			{
+				printf("\n");
+				break;
+			}
+			printf(", ");
+		}
+	}
+}
