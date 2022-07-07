8f2fe5e49f25a730581c386b27aa6bb2147d6dab
Assaletk <assaletkoua@icloud.com>
2022-07-07 11:07:17 +0000

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isupper - check for uppercase character
+*
+* @c: is a ASCII character
+*
+* Return: 1 if c is uppercase, 0 if not
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
