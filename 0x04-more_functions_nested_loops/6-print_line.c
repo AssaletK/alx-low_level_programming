f0b62c88ba090ed39fdbc74aab94363eb70b1d0c
Assaletk <assaletkoua@icloud.com>
2022-07-07 17:04:37 +0000

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_line - draw a straight line
+*
+* @n: number of times "_" is should be printed
+*
+* Return: nothing
+*/
+
+void print_line(int n)
+{
+	int i;	
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for ( i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
