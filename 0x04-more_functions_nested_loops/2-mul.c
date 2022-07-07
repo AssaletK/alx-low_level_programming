a915cb769e65f62b1acd83bf3bed6d2534d55aa2
Assaletk <assaletkoua@icloud.com>
2022-07-07 10:31:01 +0000

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - multiply tw 0 integers
+*
+* @a: is an integer
+* @b: is an integer
+*
+* Return: the result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
