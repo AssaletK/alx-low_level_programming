2062e7f1b5ef4cdffd9862d333ff2cfddee6f8e7
Assaletk <assaletkoua@icloud.com>
2022-07-07 22:54:15 +0000

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* print_line - draw a diagonal line
+*
+* @n: number of times "\" is should be printed
+*
+* Return: nothing
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
