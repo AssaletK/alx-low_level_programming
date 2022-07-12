e8e44a4d7892440dc3ff66c9db348aa721709d23
Assaletk <assaletkoua@icloud.com>
2022-07-12 05:34:58 +0000

    Pointers Arrays Strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - return the length of a string
+*
+* @s: a pointer to char
+*
+* Return: Always 0
+*/
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count ++;
+	return (count);
+}
