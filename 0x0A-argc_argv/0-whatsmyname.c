e84ef8e293ad7884c989ef82a98bd26d28241975
Assaletk <assaletkoua@icloud.com>
2022-07-26 22:25:35 +0000

    Argc Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - principal function
+*
+* @argc: int type, count arguments
+* @argv: char type, string pass in arguments
+*
+* Return: always 0
+*/
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
