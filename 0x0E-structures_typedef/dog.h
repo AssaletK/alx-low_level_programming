1fb3e1f91c20804b0047896f951568057c7bedf1
Assaletk <assaletkoua@icloud.com>
2022-08-01 20:29:50 +0000

    Structures Typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * my_dog - dog information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog
+ *
+ * Description: my dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
