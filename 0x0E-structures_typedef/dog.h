e166d85718afc806e657cd08a1bc0e557030bd16
Assaletk <assaletkoua@icloud.com>
2022-08-01 21:15:06 +0000

    Structures Typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * my_dog - dog information
- * @name: dog's name
- * @age: dog's age
- * @owner: owner of the dog
- *
- * Description: my dog information
- */
+* struct dog - dog information
+* @name: dog's name
+* @age: dog's age
+* @owner: owner of the dog
+*
+* Description: my dog information
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
+
+#endif
